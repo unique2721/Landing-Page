7b0f553802e3eebb7eb1631afc71650cfc3bef81
unique2721 <uniquehabeshawi@gmail.com>
2024-08-30 15:38:32 -0700

    #

--- virtualr/src/components/Features.jsx
+++ virtualr/src/components/Features.jsx
@@ -2,9 +2,9 @@ import { features } from "../constants/constants";
 
 const Features = () => {
   return (
-    <div className=" relative mt-20 border-b border-neutral-800 min-h-[800px]">
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
       <div className="text-center">
-        <span className=" bg-neutral-900 text-orange-500 uppercase text-sm font-medium px-2 py-1">
+        <span className=" bg-neutral-900 text-orange-500 uppercase text-sm font-medium px-2 py-1 h-6">
           Feature
         </span>
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
@@ -14,7 +14,23 @@ const Features = () => {
           </span>
         </h2>
       </div>
-      {}
+      <div className="flex flex-wrap  mt-10 lg:mt-20">
+        {features.map((feature, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="flex">
+              <div className="flex mx-6 p-2 justify-center items-center w-10 h-10  bg-neutral-900 text-orange-700 rounded-full ">
+                {feature.icon}
+              </div>
+              <div>
+                <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+                <p className="text-md mb-20 text-neutral-500">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
