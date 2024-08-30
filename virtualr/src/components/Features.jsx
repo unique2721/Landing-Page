612e1ac41177a020f7b9bf5d3fc4783caea51ebd
unique2721 <uniquehabeshawi@gmail.com>
2024-08-30 14:54:44 -0700

    #

--- virtualr/src/components/Features.jsx
+++ virtualr/src/components/Features.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { features } from "../constants/constants";
 
 const Features = () => {
-  return <div></div>;
+  return (
+    <div className=" relative mt-20 border-b border-neutral-800 min-h-[800px]">
+      <div className="text-center">
+        <span className=" bg-neutral-900 text-orange-500 uppercase text-sm font-medium px-2 py-1">
+          Feature
+        </span>
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
+          Easily build{" "}
+          <span className=" bg-gradient-to-r from-orange-500 to bg-orange-800 text-transparent bg-clip-text">
+            your code
+          </span>
+        </h2>
+      </div>
+      {}
+    </div>
+  );
 };
 
 export default Features;
