09db58e4b270e5cff6d92fbc3934a1a8da59955d
unique2721 <uniquehabeshawi@gmail.com>
2024-08-30 16:07:48 -0700

    #

--- virtualr/src/App.jsx
+++ virtualr/src/App.jsx
@@ -3,14 +3,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Workflow from "./components/Workflow";
 const App = () => {
   return (
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
+        <Features />
+        <Workflow />
       </div>
-      <Features />
     </>
   );
 };
---
+++ virtualr/src/components/Workflow.jsx
@@ -0,0 +1,37 @@
+import { CheckCircle2 } from "lucide-react";
+import codeImg from "../assets/images/code.jpg";
+import { checklistItems } from "../constants/constants";
+const Workflow = () => {
+  return (
+    <div className="mt-20">
+      <h1 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
+        Accelerate your{" "}
+        <span className=" bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          coding workflow.
+        </span>{" "}
+      </h1>
+      <div className="flex flex-wrap justify-center items-center">
+        <div className="w-full lg:w-1/2 p-2">
+          <img className=" " src={codeImg} alt="codeImg" />
+        </div>
+        <div className=" pt-12 w-full lg:w-1/2">
+          {checklistItems.map((checklistItem, index) => (
+            <div key={index} className=" flex mb-12">
+              <div className=" text-green-500 mx-6 h-10 w-10 p-2 justify-center items-center rounded-full bg-neutral-900">
+                <CheckCircle2 />
+              </div>
+              <div>
+                <h5 className="mt-1 mb-2 text-xl">{checklistItem.title}</h5>
+                <p className=" text-md text-neutral-500">
+                  {checklistItem.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Workflow;
