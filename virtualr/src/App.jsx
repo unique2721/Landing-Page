687d6aeb3a8c3beca87e6df57897bf6d6815936a
unique2721 <uniquehabeshawi@gmail.com>
2024-08-29 22:08:27 -0700

    intial commit

---
+++ virtualr/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+/* components */
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ virtualr/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+/* images */
+import { useState } from "react";
+import logo from "../assets/images/logo.png";
+/* from constants */
+import { navItems } from "../constants/constants";
+/* toggle menu icons */
+import { Menu, X } from "lucide-react";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const toggleButton = () => {
+    setToggleMenu(!toggleMenu);
+  };
+  return (
+    <nav className=" sticky top-0 backdrop-blur-lg z-50 py-3 border-b border-neutral-700/80">
+      <div className="container relative mx-auto text-sm px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="w-10 h-10 mr-2" src={logo} alt="logo" />
+            <p className="text-xl tracking-tight">VirtualR</p>
+          </div>
+          <ul className=" hidden lg:flex ml-14 space-x-12">
+            {navItems.map((navItem, i) => (
+              <li key={i}>
+                <a href={navItem.href}>{navItem.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden lg:flex justify-center items-center space-x-12">
+            <a className="py-2 px-3 border rounded-md" href="#">
+              Sign in
+            </a>
+            <a
+              className="py-2 px-3 bg-gradient-to-r from-orange-500 to-orange-800 rounded-md"
+              href="#"
+            >
+              Create an account
+            </a>
+          </div>
+        </div>
+        <div className="lg:hidden md:flex flex-col justify-end">
+          <button onClick={toggleButton}>
+            {toggleMenu ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
