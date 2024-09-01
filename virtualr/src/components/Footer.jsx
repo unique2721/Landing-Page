d2200cbee3219bf7cc1e8817198d9dba64dfea60
unique2721 <uniquehabeshawi@gmail.com>
2024-08-31 19:16:56 -0700

    #

--- virtualr/src/components/Footer.jsx
+++ virtualr/src/components/Footer.jsx
@@ -9,9 +9,12 @@ const Footer = () => {
       <div className=" grid grid-cols-2 lg:grid-cols-3 gap-4">
         <div>
           <h3 className="mb-4">Resources</h3>
-          <ul className="flex flex-col tedxt  text-neutral-300">
+          <ul className="flex flex-col tedxt  text-neutral-400">
             {resourcesLinks.map((resourcesLink, index) => (
-              <a className="mb-2" href={resourcesLink.href}>
+              <a
+                className="mb-2 hover:text-neutral-200"
+                href={resourcesLink.href}
+              >
                 {resourcesLink.text}
               </a>
             ))}
@@ -19,9 +22,12 @@ const Footer = () => {
         </div>
         <div>
           <h3 className="mb-4">Platform</h3>
-          <ul className="flex flex-col tedxt  text-neutral-300">
+          <ul className="flex flex-col tedxt  text-neutral-400">
             {platformLinks.map((platformLink, index) => (
-              <a className="mb-2" href={platformLink.href}>
+              <a
+                className="mb-2 hover:text-neutral-200"
+                href={platformLink.href}
+              >
                 {platformLink.text}
               </a>
             ))}
@@ -29,15 +35,30 @@ const Footer = () => {
         </div>
         <div>
           <h3 className="mb-4">Community</h3>
-          <ul className="flex flex-col tedxt  text-neutral-300">
+          <ul className="flex flex-col tedxt  text-neutral-400">
             {communityLinks.map((communitytLink, index) => (
-              <a className="mb-2" href={communitytLink.href}>
+              <a
+                className="mb-2 hover:text-neutral-200"
+                href={communitytLink.href}
+              >
                 {communitytLink.text}
               </a>
             ))}
           </ul>
         </div>
       </div>
+      <h2 className="my-10 text-center italic tracking-wider">
+        Developed by Gebremariam Kidane copy &copy; 2024{" "}
+        <span>
+          <a
+            href="https://github.com/unique2721"
+            target="_blank"
+            title="Click here"
+          >
+            Github
+          </a>
+        </span>
+      </h2>
     </footer>
   );
 };
