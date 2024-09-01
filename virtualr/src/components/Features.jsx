8533bc5ded47d034f3e30cab1cebcd6b8e961f91
unique2721 <uniquehabeshawi@gmail.com>
2024-08-31 19:18:34 -0700

    #

--- virtualr/src/components/Features.jsx
+++ virtualr/src/components/Features.jsx
@@ -7,10 +7,10 @@ const Features = () => {
       className="relative mt-20 border-b border-neutral-800 min-h-[800px]"
     >
       <div className="text-center">
-        <span className=" bg-neutral-900 text-orange-500 uppercase text-sm font-medium px-2 py-1 h-6">
+        <span className=" bg-neutral-900 text-orange-500 uppercase text-lg font-medium px-2 py-1 h-6 tracking-wider">
           Feature
         </span>
-        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
+        <h2 className="text-3xl sm:text-4xl lg:text-5xl mt-10 lg:mt-20 tracking-wide">
           Easily build{" "}
           <span className=" bg-gradient-to-r from-orange-500 to bg-orange-800 text-transparent bg-clip-text">
             your code
